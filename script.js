8bd719655e8e60ebe6f3c39c1ddc6074e58ae310
phos-dev <pedro.o.silva.henrique@gmail.com>
2020-09-20 22:43:31 -0300

    Add game feature added

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const fetch = require('node-fetch');
 const IGDB_API_KEY = 'd0205a3f20063d4b4779d67d81a09875';
+const imageToBase64 = require('image-to-base64');
+let sess;
 const db = require('knex')({
     client: 'pg',
     version: '7.2',
@@ -16,27 +19,57 @@ const db = require('knex')({
     }
   });
 
+
 app.use(cors());
+app.use(session({ secret: 'ssshhh',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
   
+
 const validateEmail = email => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+}
+const isLoggedIn = id => {
+    try {
+        if(id != sess.user.id) {
+            throw new Error();
+        }
+        return true;
+    }
+    catch {
+        return false;
+    }
+}
+
+const gameExists = (userId, name) => {
+    const exists = db.select('name', 'created_by').from('games')
+        .where({
+            created_by: userId,
+            name: name
+        })
+        .then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.log('Ops... an error occurred')
+        });
+    return exists;
 }
 
 
 app.get('/', (req, res) => {
     db.select('*').from('users').then(data => {
-        res.send(data);
+        res.json(data);
     });
 })
 
 app.post('/search', (req, res) => {
-   
-    console.log(req.body.name); 
     const {name} = req.body;
     fetch("https://api-v3.igdb.com/games", 
     {
@@ -51,26 +84,64 @@ app.post('/search', (req, res) => {
     .then(data => res.status(200).json(data))
     .catch(err => res.status(400).json('Ops... cannot search.'));
 });
-
 app.post('/login', (req, res) => {
     const {login, password} = req.body;
     const check = validateEmail(login) ? 'email' : 'username';
-    
+    sess = req.session;
     db.select(check, 'hash').from('login')
         .where(check, '=', login)
         .then(data => {
             const match = bcrypt.compareSync(password, data[0].hash);
-            console.log(match);
             if(match) {
                 return db.select('*').from('users')
+                    .where(check, '=', login)
                     .then(user => {
                         res.status(200).json('LOGIN_SUCCESS');
+                        sess.user = user[0];
                     })
                     .catch(err => res.status(400).json('Login Failed'))
             }
         })
         .catch(err => res.status(400).json('Wrong credentials'));
 
+});
+app.post('/add/game', (req, res) => {
+    const {name, img, description} = req.body;
+    let image;
+    imageToBase64(img) 
+    .then(res => {
+        image = res;
+    })
+    .catch(err => {
+        console.log('rear')
+    })
+    try {
+        const gE = gameExists(sess.user.id, name);
+        gE.then(data => {
+            if(data.length == 0) {
+                db.transaction(trx => {
+                    trx.insert({
+                        name: name, 
+                        image: image,
+                        description: description,
+                        created_by: sess.user.id
+                    })
+                    .into('games')
+                    .returning('name')
+                    .then(name => res.status(200).json(`Game '${name}' added`))
+                    .then(trx.commit)
+                    .catch(trx.rollback);
+                })
+                .catch(err => res.status(400).json('Unable to add.'));
+            }
+            else {  
+                res.status(400).json(`You already have a game named '${name}'!`);
+            }
+        })
+    }
+    catch {
+        res.status(400).json('Not logged in.');
+    }
 })
 app.post('/register', (req, res) => {
     const {name, username, email, password} = req.body;
@@ -94,7 +165,7 @@ app.post('/register', (req, res) => {
                             joined: new Date()
                         })
                         .then(user => {
-                            res.json(user[0]);
+                            res.status(200).json(user[0]);
                         })
                 })
                 .then(trx.commit)
@@ -104,12 +175,12 @@ app.post('/register', (req, res) => {
         }
     });
 })
-app.put('/profile/:id', (req, res) => {
-    if(req.body.user === users[0].user && req.body.password === users[0].password){
-        res.status(200).send('success');
+app.get('/profile/:id', (req, res) => {
+    if(isLoggedIn(req.params.id)) {
+        res.status(200).json(sess.user);
     }
     else {
-        res.status(400).send('failed');
+        res.status(400).json('Not logged in.');
     }
 })
 app.listen(3001);
