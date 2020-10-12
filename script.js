c07129826ee418ad72a419f215cbb521f15db063
Pedro Henrique <38169759+phos-dev@users.noreply.github.com>
2020-10-11 23:49:41 -0300

    Changing to heroku

--- script.js
+++ script.js
@@ -52,6 +52,11 @@ app.use(session({ secret: 'ssshhh',
 		httpOnly: true
     }
 }));
+app.use(function(req, res, next) {
+    if ((req.get('X-Forwarded-Proto') !== 'https')) {
+      res.redirect('https://' + req.get('Host') + req.url);
+    } else next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 auth(db, app);
